c5e95d4c9ef5428aa05585608c629c9500a351c0
nobuyuki nagato <n.nagagto726@gmail.com>
2022-07-08 12:06:52 +0000

    final commit

--- practice.php
+++ practice.php
@@ -2,37 +2,36 @@
 echo 'hello php!';
 
 //課題1 
-//
-//$a = 3;
-//$b = 7;
-//echo $a + $b;
-//var_dump($a + $b);
-//
-//// 課題2
-//
-//$array_month = [
-//    "January" => "1月",
-//        "February" => "2月",
-//            "March" => "3月",
-//                "April" => "4月",
-//                    "May" => "5月",
-//                        "June" => '6月',
-//                            "July" => "7月",
-//                                "August" => "8月",
-//                                    "September" => "9月",
-//                                        "October" => "10月",
-//                                            "November" => "11月",
-//                                                "December" =>"12月" ];
-//                                                    
-//                                                    echo $array_month["Augusat"];
-//                                                    var_dump($array_month["August"]);
-//
-//                                                    // 課題３
-//                                                    $hello = "Hello, ";
-//                                                    $name = "nobuyuki";
-//                                                    $world = "'s world";
-//                                                    $combination = $hello. $name. $world ."from Japan";
-//
-//                                                    echo $hello. $name. $world;
-//                                                    var_dump($combination);
-//                                                    
+
+$a = 3;
+$b = 7;
+echo $a + $b;
+var_dump($a + $b);
+
+// 課題2
+
+$array_month = [
+    "January" => "1月",
+    "February" => "2月",
+    "March" => "3月",
+    "April" => "4月",
+    "May" => "5月",
+    "June" => '6月',
+    "July" => "7月",
+    "August" => "8月",
+    "September" => "9月",
+    "October" => "10月",
+    "November" => "11月",
+    "December" =>"12月" ];
+    
+echo $array_month["Augusat"];
+var_dump($array_month["August"]);
+
+// 課題３
+$hello = "Hello, ";
+$name = "nobuyuki";
+$world = "'s world";
+$combination = $hello. $name. $world ."from Japan";
+
+echo $hello. $name. $world;
+var_dump($combination);
